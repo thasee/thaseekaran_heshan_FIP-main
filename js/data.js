da562aa45fec1627701afdf47693e16ac4a1d787
thasee <67984831+thasee@users.noreply.github.com>
2024-06-19 13:30:49 -0400

    updates

---
+++ js/data.js
@@ -0,0 +1,71 @@
+export const heroSectionImages = [
+  "pexels-polina-kovaleva-7248416.jpg",
+  "pexels-badulescu-badulescu-1138694-2171077.jpg",
+  "pexels-didsss-2183113.jpg",
+  "pexels-fabien-burgue-1052232-6249894.jpg",
+  "pexels-bella-white-201200-1289256.jpg",
+  "pexels-fabien-burgue-1052232-6249894.jpg",
+];
+
+export const productCarousel = [
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+  {
+    title: "Mango",
+    imageText: "Dr Nut",
+  },
+];
